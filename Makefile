31c683583ba950e3324e17058e5a7c6911f5e81b
James <james@localhost.localdomain>
2015-12-11 23:45:44 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,35 @@
+include $(TOPDIR)/rules.mk
+
+PKG_NAME:=gluon-ffbi-map
+PKG_VERSION:=1
+PKG_RELEASE:=1
+
+PKG_BUILD_DIR := $(BUILD_DIR)/$(PKG_NAME)
+
+include $(INCLUDE_DIR)/package.mk
+
+define Package/gluon-ffbi-map
+  SECTION:=gluon
+  CATEGORY:=Gluon
+  TITLE:=gluon-ffbi-map
+  DEPENDS:=+gluon-core
+endef
+
+define Package/gluon-ffbi-map/description
+	Gluon Freifunk Bielefeld alfred map scripts
+endef
+
+define Build/Prepare
+endef
+
+define Build/Configure
+endef
+
+define Build/Compile
+endef
+
+define Package/gluon-ffbi-map/install
+	$(CP) ./files/* $(1)/
+endef
+
+$(eval $(call BuildPackage,gluon-ffbi-map))
---
+++ files/usr/sbin/print_map.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+#Print out local connection data for map creation
+
+print() {
+	local community="$(uci get -q freifunk.@settings[0].community 2> /dev/null)"
+	local version="$(cat /lib/gluon/release 2> /dev/null)"
+	local name="$(uci get -q system.@system[0].hostname 2> /dev/null)"
+	local geo="$(uci get -q gluon-node-info.@location[0].latitude 2> /dev/null) $(uci get -q gluon-node-info.@location[0].longitude 2> /dev/null)"
+	local contact="$(uci get -q gluon-node-info.@owner[0].contact 2> /dev/null)"
+
+	echo -n "{"
+
+	[ -n "$geo" ] && echo -n "\"geo\" : \"$geo\", "
+	[ -n "$name" ] && echo -n "\"name\" : \"$name\", "
+	[ -n "$contact" ] && echo -n "\"contact\" : \"$contact\", "
+	[ -n "$version" ] && echo -n "\"firmware\" : \"ffbi-$version\", "
+	[ -n "$community" ] && echo -n "\"community\" : \"$community\", "
+
+	echo -n "\"links\" : ["
+
+	printLink() { echo -n "{ \"smac\" : \"$(cat /sys/class/net/$3/address)\", \"dmac\" : \"$1\", \"qual\" : $2 }"; }
+	IFS="
+"
+	nd=0
+	for entry in $(cat /sys/kernel/debug/batman_adv/bat0/originators 2> /dev/null | tr '\t/[]()' ' ' |  awk '{ if($1==$4) print($1, $3, $5) }'); do
+		[ $nd -eq 0 ] && nd=1 || echo -n ", "
+		IFS=" "
+		printLink $entry
+	done
+
+	echo -n '], '
+	mac=$(uci get -q network.bat0.macaddr)
+	cat /sys/kernel/debug/batman_adv/bat0/transtable_local 2> /dev/null | tr '\t/[]()' ' ' | awk -v mac=$mac 'BEGIN{ c=0; } { if($1 == "*" && $2 != mac && $4 ~ /^[.NW]+$/ && $5 < 300) c++;} END{ printf("\"clientcount\" : %d", c);}'
+	echo -n '}'
+}
+
+if [ "$1" = "-p" ]; then
+	publish_map="$(uci get -q gluon-node-info.@location[0].share_location 2> /dev/null)"
+	[ "$publish_map" != "1" ] && exit 0
+
+	content="$(print)"
+	if [ -n "$content" ]; then
+		#make sure alfred is running
+		pidof alfred > /dev/null || /etc/init.d/alfred start
+
+		#publish content via alfred
+		echo "$content" | alfred -s 64
+		echo "map published"
+	else
+		echo "nothing published"
+	fi
+else
+	print
+fi
