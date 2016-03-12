b3a02922cfa82c3bdab4ca4ae71d2af17f64958d
JamesT42 <j.tuvix@gmx.de>
2016-03-12 16:18:41 +0100

    New version for meshviewer

--- files/usr/sbin/print_map.sh
+++ files/usr/sbin/print_map.sh
@@ -2,16 +2,33 @@
 
 #Print out local connection data for map creation
 
-print() {
+memory_usage()
+{
+	meminfo=$(cat /proc/meminfo)
+	free=$(echo "$meminfo" | awk /^MemFree:/'{print($2)}')
+	buffers=$(echo "$meminfo" | awk /^Buffers:/'{print($2)}')
+	cached=$(echo "$meminfo" | awk /^Cached:/'{print($2)}')
+	total=$(echo "$meminfo" | awk /^MemTotal:/'{print($2)}')
+	echo $free $buffers $cached $total | awk '{ printf("%f", 1 - ($1 + $2 + $3) / $4)}'
+}
+
+rootfs_usage()
+{
+	df / | awk '/^rootfs/{print($5/100)}'
+}
+
+print_basic() {
 	local community="$(uci get -q freifunk.@settings[0].community 2> /dev/null)"
 	local version="gluon-$(cat /lib/gluon/release 2> /dev/null)"
 	local name="$(uci get -q system.@system[0].hostname 2> /dev/null)"
-	local geo="$(uci get -q gluon-node-info.@location[0].latitude 2> /dev/null) $(uci get -q gluon-node-info.@location[0].longitude 2> /dev/null)"
+	local longitude="$(uci get -q gluon-node-info.@location[0].longitude 2> /dev/null)"
+	local latitude="$(uci get -q gluon-node-info.@location[0].latitude 2> /dev/null)"
+	#local geo="$(uci get -q gluon-node-info.@location[0].latitude 2> /dev/null) $(uci get -q gluon-node-info.@location[0].longitude 2> /dev/null)"
 	local contact="$(uci get -q gluon-node-info.@owner[0].contact 2> /dev/null)"
 
 	echo -n "{"
 
-	[ -n "$geo" ] && echo -n "\"geo\" : \"$geo\", "
+	#[ -n "$geo" ] && echo -n "\"geo\" : \"$geo\", "
 	[ -n "$name" ] && echo -n "\"name\" : \"$name\", "
 	[ -n "$contact" ] && echo -n "\"contact\" : \"$contact\", "
 	[ -n "$version" ] && echo -n "\"firmware\" : \"$version\", "
@@ -34,6 +51,32 @@ print() {
 	cat /sys/kernel/debug/batman_adv/bat0/transtable_local 2> /dev/null | tr '\t/[]()' ' ' | awk -v mac=$mac 'BEGIN{ c=0; } { if($1 == "*" && $2 != mac && $4 ~ /^[.NW]+$/ && $5 < 300) c++;} END{ printf("\"clientcount\" : %d", c);}'
 	echo -n '}'
 }
+
+print_more() {
+	echo -n "\"loadavg\" : $(uptime | awk '{print($NF)}'), "
+	echo -n "\"uptime\" : $(cat /proc/uptime | awk '{print($1)}'), "
+	echo -n "\"model\" : \"$(cat /tmp/sysinfo/model)\", "
+
+	print_basic
+}
+
+print_all() {
+	echo -n "\"rootfs_usage\" : $(rootfs_usage), "
+	echo -n "\"memory_usage\" : $(memory_usage), "
+	echo -n "\"addresses\" : ["
+	ip -6 address show dev br-freifunk 2> /dev/null | tr '/' ' ' | awk 'BEGIN{i=0} /inet/ { if($2 !~ /^fe80/) { printf("%s\"%s\"", (i ? ", " : ""), $2); i=1; }}'
+	echo -n "], "
+
+	print_more
+}
+
+print() {
+
+	print_all
+	echo -n '}'
+
+}
+
 
 if [ "$1" = "-p" ]; then
 	publish_map="$(uci get -q gluon-node-info.@location[0].share_location 2> /dev/null)"
